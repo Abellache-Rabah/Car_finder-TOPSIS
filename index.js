cbf8c97000e2a192f9ae1cabb092a25bd6c90400
Rabah Abellache <agar3arab@gmail.com>
2024-03-27 17:54:28 +0000

    hosting server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 
 const app = express();
-const port = 3000; // Adjust as needed
+const port = process.env.PORT || 3000; // Adjust as needed
 
 // Connect to MySQL database
 const pool = mysql.createPool({
